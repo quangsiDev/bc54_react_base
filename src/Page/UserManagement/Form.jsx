88a7557cb8d1da83d5d2769e5831f66bc541ed91
quangsiDev <tranquangsidev@gmail.com>
2023-11-24 19:13:44 +0700

    update

--- src/Page/UserManagement/Form.jsx
+++ src/Page/UserManagement/Form.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
-  return <div>Form</div>;
+  const [formValue, setFormValue] = useState({
+    name: "tommy",
+    account: "tomyyyyy",
+    password: "123",
+  });
+  let handleChangeForm = (e) => {
+    console.log(e.target.name);
+  };
+  return (
+    <div>
+      <input
+        name="name"
+        value={formValue.name}
+        type="text"
+        class="form-control"
+        placeholder="Name"
+      />
+      <input
+        name="account"
+        value={formValue.account}
+        type="text"
+        class="form-control"
+        placeholder="Account"
+      />
+      <input
+        name="password"
+        value={formValue.password}
+        type="text"
+        class="form-control"
+        placeholder="Password"
+      />
+      <button className="btn btn-primary">Add</button>
+    </div>
+  );
 }
